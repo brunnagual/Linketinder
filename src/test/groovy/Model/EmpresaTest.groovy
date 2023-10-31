366f7699c30c66dddd5bcd3202d04034051978ca
brunnagual <brunnagual@gmail.com>
2023-10-30 23:21:49 -0300

    feat(test): Adicionando o teste para classe Empresa

--- src/test/groovy/Model/EmpresaTest.groovy
+++ src/test/groovy/Model/EmpresaTest.groovy
@@ -1,28 +1,28 @@
-//package Model
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.Test
-//
-//class EmpresaTest {
-//
-//    private List<Empresa> empresas;
-//
-//    @BeforeEach
-//    void setUp() {
-//        empresas = new ArrayList<>();
-//    }
-//
-//    @Test
-//    void testAdicionarEmpresa() {
-//        Scanner scanner = new Scanner("EmpresaXYZ\ncontato@empresa.com\n12345678\n12345678901234\nTestando cadastro");
-//
-//        Empresa.cadastrarEmpresa(empresas, scanner);
-//
-//        Assertions.assertEquals("EmpresaXYZ", empresas.get(0).nome);
-//        Assertions.assertEquals("contato@empresa.com", empresas.get(0).email);
-//        Assertions.assertEquals("12345678", empresas.get(0).cep);
-//        Assertions.assertEquals("12345678901234", empresas.get(0).cnpj);
-//        Assertions.assertEquals("Testando cadastro", empresas.get(0).descricao);
-//    }
-//}
+package Model
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class EmpresaTest {
+
+    private List<EmpresaModel> empresas;
+
+    @BeforeEach
+    void setUp() {
+        empresas = new ArrayList<>();
+    }
+
+    @Test
+    void testAdicionarEmpresa() {
+        EmpresaModel empresa = new EmpresaModel("Empresa", "joao@example.com", "12345678",
+                "12345759000105", "Cadasrtrando empresa");
+        empresas.add(empresa);
+
+        Assertions.assertEquals("Empresa", empresas.get(0).nome);
+        Assertions.assertEquals("joao@example.com", empresas.get(0).email);
+        Assertions.assertEquals("12345678", empresas.get(0).cep);
+        Assertions.assertEquals("12345759000105", empresas.get(0).cnpj);
+        Assertions.assertEquals("Cadasrtrando empresa", empresas.get(0).descricao);
+    }
+}
