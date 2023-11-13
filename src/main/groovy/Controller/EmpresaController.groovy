c38599a5e0a1893adbe54ef9a75e26cb5e53f0fa
Brunna Gualberto <brunnagual@gmail.com>
2023-11-13 20:46:52 -0300

    Feat: Inserindo endpoint nas classes Candidato, Empresa e Vaga

--- src/main/groovy/Controller/EmpresaController.groovy
+++ src/main/groovy/Controller/EmpresaController.groovy
@@ -1,11 +1,71 @@
 package Controller
-import DAO.ConexaoDAO
 import DAO.EmpresaDAO
 import Model.EmpresaModel
-
+import DAO.ConexaoDAO
 import java.sql.Connection
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import com.google.gson.Gson
 
-class EmpresaController {
+@WebServlet("/empresa")
+class EmpresaController extends HttpServlet {
+    private static final long serialVersionUID = 1L
+    private Gson gson = new Gson()
+
+    EmpresaDAO empresaDao = new EmpresaDAO()
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<EmpresaModel> empresas = empresaDao.listarEmpresas()
+
+            String jsonEmpresas = convertToJSON(empresas);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(jsonEmpresas);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao obter a lista de empresas.");
+        }
+    }
+
+    private static String convertToJSON(List<EmpresaModel> empresas) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(empresas);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String corpoRequisicao = request.getReader().lines().collect(java.util.stream.Collectors.joining(System.lineSeparator()));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            EmpresaModel empresa = objectMapper.readValue(corpoRequisicao, EmpresaModel.class);
+
+            boolean sucesso = cadastrarEmpresa(empresa);
+
+            if (sucesso) {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.getWriter().write("Empresa inserida com sucesso");
+            } else {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Erro ao inserir empresa");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Erro ao inserir empresa");
+        }
+    }
 
     static boolean cadastrarEmpresa(EmpresaModel empresa, Scanner scanner) {
         Connection con = ConexaoDAO.getInstance().getConnection()
--- src/main/groovy/Controller/VagasController.groovy
+++ src/main/groovy/Controller/VagasController.groovy
@@ -1,13 +1,73 @@
 package Controller
 
-
-import DAO.ConexaoDAO
 import DAO.VagasDAO
 import Model.VagasModel
-
+import DAO.ConexaoDAO
 import java.sql.Connection
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.ObjectMapper
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import com.google.gson.Gson
 
-class VagasController {
+@WebServlet("/vaga")
+class VagasController extends HttpServlet{
+    private static final long serialVersionUID = 1L
+    private Gson gson = new Gson()
+
+    VagasDAO vagasDao = new VagasDAO()
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            List<VagasModel> vagas = vagasDao.listarVagas()
+
+            String jsonVagas = convertToJSON(vagas);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(jsonVagas);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Erro ao obter a lista de vagas.");
+        }
+    }
+
+    private static String convertToJSON(List<VagasModel> vagas) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(vagas);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String corpoRequisicao = request.getReader().lines().collect(java.util.stream.Collectors.joining(System.lineSeparator()));
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            VagasModel vaga = objectMapper.readValue(corpoRequisicao, VagasModel.class);
+
+            int vagaId = cadastrarVaga(vaga);
+
+            if (vagaId != -1) {
+                response.setStatus(HttpServletResponse.SC_CREATED);
+                response.getWriter().write("Vaga inserida com sucesso");
+            } else {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().write("Erro ao inserir vaga");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Erro ao inserir vaga");
+        }
+    }
+
 
     static Double cadastrarVaga(VagasModel vaga, Scanner scanner) {
         Connection con = ConexaoDAO.getInstance().getConnection()
